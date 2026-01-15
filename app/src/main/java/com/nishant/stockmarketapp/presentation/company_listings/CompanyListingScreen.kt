9f07c42f1adcbe1345b423f444c0776d60c6567f
nishant <nishantpriya.scorpion@gmail.com>
2026-01-15 20:50:19 +0530

    screens

--- app/src/main/java/com/nishant/stockmarketapp/presentation/company_listings/CompanyListingScreen.kt
+++ app/src/main/java/com/nishant/stockmarketapp/presentation/company_listings/CompanyListingScreen.kt
@@ -1,9 +1,62 @@
 package com.nishant.stockmarketapp.presentation.company_listings
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.Navigator
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Composable
+@Destination(start = true)
 fun CompanyListingScreen(
-    navigator:
-)
+    navigator: DestinationsNavigator,
+    viewModel: CompanyListingsViewModel = hiltViewModel()
+
+){
+    val swipeRefreshstate = rememberSwipeRefreshState(
+        isRefreshing = viewModel.state.isRefreshing
+    )
+
+    val state = viewModel.state
+
+    Column (modifier = Modifier.fillMaxSize()){
+        OutlinedTextField(value = state.searchQuery, onValueChange = {
+            viewModel.onEvent(CompanyListingEvent.OnSearchQueryChange(query = it))
+        },
+            modifier = Modifier.padding(16.dp).fillMaxWidth(),
+
+            placeholder = {
+                Text("search...")
+            },
+            maxLines = 1,
+            singleLine = true,
+
+            )
+
+        SwipeRefresh(state = swipeRefreshstate, onRefresh = {
+            viewModel.onEvent(CompanyListingEvent.Refresh)
+        }) {
+            LazyColumn (modifier = Modifier.fillMaxSize()){
+                items(state.companies.size){
+                    i ->
+
+                    if(i < state.companies.size){
+                        Divider()
+                    }
+                }
+            }
+        }
+    }
+}
